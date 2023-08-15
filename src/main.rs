fc7d3a677a46886f03d2ef46d9a960266eab7e3a
Aivel <aivellinsom@gmail.com>
2023-08-15 23:20:27 +0400

    PWM for LED works. i2c doesn't

--- src/main.rs
+++ src/main.rs
@@ -8,22 +8,24 @@ use rtic::app;
 use panic_rtt_target as _;
 
 
-
-
-#[app(device = stm32f1xx_hal::stm32)]
+#[app(device = stm32f1xx_hal::pac, peripherals = true, dispatchers = [TIM2])]
 mod app {
+    use am2320::Am2320;
     use rtt_target::{rprintln, rtt_init_print};
-    use stm32f1xx_hal::{pac, prelude::*};
+    use stm32f1xx_hal::{pac, prelude::*, i2c::{BlockingI2c, DutyCycle}, timer::{Timer, Tim2NoRemap, Tim2PartialRemap2, Timer2, Channel}, device::TIM2};
     use systick_monotonic::Systick;
 
     const CLOCK_HZ: u32 = 72_000_000;
 
     #[shared]
     struct Shared {
+        am2320: Am2320<BlockingI2c<stm32f1xx_hal::device::I2C1, (stm32f1xx_hal::gpio::Pin<'B', 6, stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::OpenDrain>>, stm32f1xx_hal::gpio::Pin<'B', 7, stm32f1xx_hal::gpio::Alternate<stm32f1xx_hal::gpio::OpenDrain>>)>, stm32f1xx_hal::timer::Delay<stm32f1xx_hal::device::TIM3, 1000000>>,
     }
 
     #[local]
     struct Local {
+        led_pwm: stm32f1xx_hal::timer::PwmHz<TIM2, Tim2NoRemap, (stm32f1xx_hal::timer::Ch<0>, stm32f1xx_hal::timer::Ch<1>, stm32f1xx_hal::timer::Ch<2>, stm32f1xx_hal::timer::Ch<3>), (stm32f1xx_hal::gpio::Pin<'A', 0, stm32f1xx_hal::gpio::Alternate>, stm32f1xx_hal::gpio::Pin<'A', 1, stm32f1xx_hal::gpio::Alternate>, stm32f1xx_hal::gpio::Pin<'A', 2, stm32f1xx_hal::gpio::Alternate>, stm32f1xx_hal::gpio::Pin<'A', 3, stm32f1xx_hal::gpio::Alternate>)>
+        // bme: AM2320<BlockingI2c<I2C1, (SCL, SDA)>>,
     }
 
     #[monotonic(binds = SysTick, default = true)]
@@ -46,11 +48,57 @@ mod app {
         let mut flash = pac.FLASH.constrain();
         let mut rcc = pac.RCC.constrain();
         let clocks = rcc
-            .cfgr
-            .use_hse(8.MHz())
-            .sysclk(CLOCK_HZ.Hz())
-            .pclk1(24.MHz())
-            .freeze(&mut flash.acr);
+        .cfgr.use_hse(8.MHz())  // use external oscillator (8 MHz)
+        .sysclk(72.MHz())  // system clock, PLL multiplier should be 6
+        .hclk(8.MHz())     // clock used for timers
+        .freeze(&mut flash.acr);
+
+        let mut gpioa = pac.GPIOA.split();
+        let mut afio = pac.AFIO.constrain();
+        // led
+        let p1 = gpioa.pa0.into_alternate_push_pull(&mut gpioa.crl);
+        let p2 = gpioa.pa1.into_alternate_push_pull(&mut gpioa.crl);
+        let p3 = gpioa.pa2.into_alternate_push_pull(&mut gpioa.crl);
+        let p4 = gpioa.pa3.into_alternate_push_pull(&mut gpioa.crl);
+
+        let pins = (p1, p2, p3, p4);
+
+        let mut led_pwm: stm32f1xx_hal::timer::PwmHz<TIM2, Tim2NoRemap, (stm32f1xx_hal::timer::Ch<0>, stm32f1xx_hal::timer::Ch<1>, stm32f1xx_hal::timer::Ch<2>, stm32f1xx_hal::timer::Ch<3>), (stm32f1xx_hal::gpio::Pin<'A', 0, stm32f1xx_hal::gpio::Alternate>, stm32f1xx_hal::gpio::Pin<'A', 1, stm32f1xx_hal::gpio::Alternate>, stm32f1xx_hal::gpio::Pin<'A', 2, stm32f1xx_hal::gpio::Alternate>, stm32f1xx_hal::gpio::Pin<'A', 3, stm32f1xx_hal::gpio::Alternate>)> = pac
+        .TIM2
+        .pwm_hz::<Tim2NoRemap, _, _>(pins, &mut afio.mapr, 1000.Hz(), &clocks);
+
+        // Enable clock on each of the channels
+        led_pwm.enable(stm32f1xx_hal::timer::Channel::C1);
+        let max_duty = led_pwm.get_max_duty();
+        led_pwm.set_duty(Channel::C1, max_duty);
+
+        rprintln!("max duty - {}", led_pwm.get_max_duty());
+
+        let mut delay = pac.TIM3.delay_us(&clocks);
+
+        
+        // Acquire the GPIOB peripheral
+        let mut gpiob = pac.GPIOB.split();
+
+        let am2320_scl = gpiob.pb6.into_alternate_open_drain(&mut gpiob.crl);
+        let am2320_sda = gpiob.pb7.into_alternate_open_drain(&mut gpiob.crl);
+
+        let am2320_i2c = BlockingI2c::i2c1(
+            pac.I2C1,
+            (am2320_scl, am2320_sda),
+            &mut afio.mapr,
+            stm32f1xx_hal::i2c::Mode::Standard {
+                frequency: 100.kHz(),
+            },
+            clocks,
+            1000,
+            10,
+            1000,
+            1000,
+        );
+
+        let mut am2320 = Am2320::new(am2320_i2c, delay);
+
 
         // Initialize (enable) the monotonic timer (CYCCNT)
         cp.DCB.enable_trace();
@@ -59,16 +107,41 @@ mod app {
         let mono = Systick::new(cp.SYST, CLOCK_HZ);
 
         (
-            Shared {  },
-            Local {
+            Shared { am2320 },
+            Local { led_pwm
             },
             init::Monotonics(mono),
         )
     }
 
-    #[idle()]
+    #[idle(local=[led_pwm], shared=[am2320])]
     fn idle(mut cx: idle::Context) -> ! {
-        loop{}
+        let mut pwm_counter: u16 = 0;
+        let pwm = cx.local.led_pwm;
+        let mut is_up = true;
+        loop{
+            if is_up {
+                pwm_counter += 50;
+                if pwm_counter > pwm.get_max_duty() {
+                    is_up = false;
+                }
+            } else {
+                pwm_counter -= 50;
+                if pwm_counter < 1 {
+                    is_up = true;
+                }
+            }
+            pwm.set_duty(Channel::C1, pwm_counter);
+            rprintln!("duty - {}", pwm_counter);
+
+            let data = cx.shared.am2320.lock(|am2320| {
+                am2320.read()
+            });
+            
+            let measurement = data.expect("can't get measurement");
+            rprintln!("{:?}", measurement);
+            
+        }
     }
 
 }
